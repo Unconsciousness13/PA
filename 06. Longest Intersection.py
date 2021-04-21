36fe1244065a88c6c7ba389cef96f5cd90aa7612
Plamen Iliev <77366596+Unconsciousness13@users.noreply.github.com>
2021-04-21 14:57:55 +0200

    Tuples and Sets - Exercise

---
+++ 06. Longest Intersection.py
@@ -0,0 +1,14 @@
+n = int(input())
+intersecttions = []
+for _ in range(n):
+    data = input()
+    first_seq_data, second_seq_data = data.split('-')
+    start, stop = [int(el) for el in first_seq_data.split(',')]
+    first_seq = range(start, stop + 1)
+    start, stop = [int(el) for el in second_seq_data.split(',')]
+    second_seq = range(start, stop + 1)
+    intersection = set(first_seq).intersection(second_seq)
+    intersecttions.append(intersection)
+longest = sorted(intersecttions, key=lambda x: -len(x))[0]
+print(f"Longest intersection is {list(longest)} with length {len(longest)}")
+
