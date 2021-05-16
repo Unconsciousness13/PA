c932541a206673835de2ec091846df5e798cd5a3
Plamen Iliev <77366596+Unconsciousness13@users.noreply.github.com>
2021-05-16 19:43:42 +0200

    Functions Advanced - Lab

---
+++ 05. Person Info.py
@@ -0,0 +1,4 @@
+#def get_info(name, age, town):
+    #return f"This is {name} from {town} and he is {age} years old"
+def get_info(**kwargs):
+    return f"This is {kwargs.get('name')} from {kwargs.get('town')} and he is {kwargs.get('age')} years old"
