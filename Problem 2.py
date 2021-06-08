0dc0cb153dbd8d9cdd80e29213f8c3002637042a
Plamen Iliev <77366596+Unconsciousness13@users.noreply.github.com>
2021-06-08 14:10:46 +0200

    Python Advanced Retake Exam - 16 Dec 2020

--- Problem 2.py
+++ Problem 2.py
@@ -1,88 +1,67 @@
-import math
-
+text = input()
 n = int(input())
-coins = 0
-path = []
+matrix = [[el for el in input()] for rows in range(n)]
+m = int(input())
 row = 0
 col = 0
-matrix = [[i for i in input().split(" ")] for el in range(n)]
-for r in range(len(matrix)):
-    for c in range(len(matrix)):
-        if matrix[r][c] == "P":
-            row = r
-            col = c
-            break
-current_row = row
-current_col = col
-while True:
-    if coins >= 100:
-        break
-    command = input()
 
-    if command == "left":
-        if current_col > 0 and current_row in range(len(matrix)):
-            if matrix[current_row][current_col - 1].isnumeric():
-                coins += int(matrix[current_row][current_col - 1])
-                path.append([current_row, current_col - 1])
-                current_row, current_col = current_row, current_col - 1
-                current_position = (current_row, current_col)
+
+for itt in range(m):
+    command = input()
+    for r in range(len(matrix)):
+        for c in range(len(matrix)):
+            if matrix[r][c] == "P":
+                row = r
+                col = c
+                break
+    current_row = row
+    current_col = col
+    if command == "up":
+        if current_row > 0 and current_col in range(len(matrix)):
+            if matrix[current_row - 1][current_col].isalpha():
+                text += matrix[current_row - 1][current_col]
+                matrix[current_row][current_col] = "-"
+                matrix[current_row - 1][current_col] = "P"
             else:
-                if matrix[current_row][current_col - 1] == "X":
-                    break
-                else:
-                    continue
+                matrix[current_row][current_col] = "-"
+                matrix[current_row - 1][current_col] = "P"
         else:
-            break
+            text = text[0:-1]
+
+    elif command == "down":
+        if current_row < n - 1 and current_col in range(len(matrix)):
+            if matrix[current_row + 1][current_col].isalpha():
+                text += matrix[current_row + 1][current_col]
+                matrix[current_row][current_col] = "-"
+                matrix[current_row + 1][current_col] = "P"
+            else:
+                matrix[current_row][current_col] = "-"
+                matrix[current_row + 1][current_col] = "P"
+        else:
+            text = text[0:-1]
 
     elif command == "right":
         if current_col < n - 1 and current_row in range(len(matrix)):
-            if matrix[current_row][current_col + 1].isnumeric():
-                coins += int(matrix[current_row][current_col + 1])
-                path.append([current_row, current_col + 1])
-                current_row, current_col = current_row, current_col + 1
-                current_position = (current_row, current_col)
+            if matrix[current_row][current_col + 1].isalpha():
+                text += matrix[current_row][current_col + 1]
+                matrix[current_row][current_col] = "-"
+                matrix[current_row][current_col + 1] = "P"
             else:
-                if matrix[current_row][current_col + 1] == "X":
-                    break
-                else:
-                    continue
+                matrix[current_row][current_col] = "-"
+                matrix[current_row][current_col + 1] = "P"
         else:
-            break
-
-    elif command == "up":
-        if current_row > 0 and current_col in range(len(matrix)):
-            if matrix[current_row - 1][current_col].isnumeric():
-                coins += int(matrix[current_row - 1][current_col])
-                path.append([current_row - 1, current_col])
-                current_row, current_col = current_row - 1, current_col
-                current_position = (current_row, current_col)
+            text = text[0:-1]
+    elif command == "left":
+        if current_col > 0 and current_row in range(len(matrix)):
+            if matrix[current_row][current_col - 1].isalpha():
+                text += matrix[current_row][current_col - 1]
+                matrix[current_row][current_col] = "-"
+                matrix[current_row][current_col - 1] = "P"
             else:
-                if matrix[current_row - 1][current_col] == "X":
-                    break
-                else:
-                    continue
+                matrix[current_row][current_col] = "-"
+                matrix[current_row][current_col - 1] = "P"
         else:
-            break
-    elif command == "down":
-        if current_row < n - 1 and current_col in range(len(matrix)):
-            if matrix[current_row + 1][current_col].isnumeric():
-                coins += int(matrix[current_row + 1][current_col])
-                path.append([current_row + 1, current_col])
-                current_row, current_col = current_row + 1, current_col
-                current_position = (current_row, current_col)
-            else:
-                if matrix[current_row + 1][current_col] == "X":
-                    break
-                else:
-                    continue
-        else:
-            break
-
-if coins >= 100:
-    print(f"You won! You've collected {coins} coins.")
-else:
-    coins = coins / 2
-    coins = math.floor(coins)
-    print(f"Game over! You've collected {coins} coins.")
-print('Your path:')
-print(*path, sep='\n')
+            text = text[0:-1]
+print(text)
+for el in matrix:
+    print("".join(el))
