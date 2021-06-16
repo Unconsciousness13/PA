6dc42db452bac64c5bb1983160f1e829ccc020b5
Plamen Iliev <77366596+Unconsciousness13@users.noreply.github.com>
2021-06-16 11:18:02 +0200

    Python Advanced Retake Exam - 14 April 2021

--- 03.Problem 3.py
+++ 03.Problem 3.py
@@ -1,17 +1,18 @@
-def get_magic_triangle(n):
-    matrix = []
-    number = 1
-    for num in range(1, n + 1):
-        matrix.append([number] * num)
-    for r in range(0, n):
-        for c in range(len(matrix[r])):
-            if r - 1 > 0 and c - 1 >= 0:
-                if c in range(r):
-                    matrix[r][c] = matrix[r - 1][c - 1] + matrix[r - 1][c]
-            else:
-                matrix[r][c] = matrix[0][0]
+def flights(*args):
+    result = {}
+    for arg in range(len(args)):
+        if args[arg] == "Finish":
+            break
+        if arg % 2 == 0:
+            if args[arg] not in result:
+                result[args[arg]] = 0
+            flight = args[arg]
+        else:
+            pas = args[arg]
+            result[flight] += pas
+    return result
 
-    return matrix
 
-
-get_magic_triangle(5)
+print(flights('Vienna', 256, 'Vienna', 26, 'Morocco', 98, 'Paris', 115, 'Finish', 'Paris', 15))
+print(flights('London', 0, 'New York', 9, 'Aberdeen', 215, 'Sydney', 2, 'New York', 300, 'Nice', 0, 'Finish'))
+print(flights('Finish', 'New York', 90, 'Aberdeen', 300, 'Sydney', 0))
